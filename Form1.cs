eb90e795f4658fc8e26c60871f2cfb804e9dc2c1
Alexey Obukhov (Алексей Обухов) <miptleha@gmail.com>
2021-06-19 18:36:50 +0300

    translate

--- Form1.cs
+++ Form1.cs
@@ -25,7 +25,7 @@ namespace WinFormsApp1
         {
             using (var g = this.CreateGraphics())
             {
-                //рисуем в буфере, чтоб не было моргания
+                //use buffering to avoid blinking
                 Bitmap buffer = new Bitmap(this.Width, this.Height);
                 Graphics g1 = Graphics.FromImage(buffer);
                 g1.SmoothingMode = SmoothingMode.HighSpeed;
@@ -33,7 +33,7 @@ namespace WinFormsApp1
                 for (double shift = 0; ; shift += .05)
                 {
                     g1.Clear(this.BackColor);
-                    DrawF(g1, Math.Sin, 100, 100 * Math.PI, shift); //рисуем функцию со смещением по x (эффект движения вправо)
+                    DrawF(g1, Math.Sin, 100, 100 * Math.PI, shift); //shift right
 
                     try
                     {
@@ -41,37 +41,37 @@ namespace WinFormsApp1
                     }
                     catch
                     {
-                        //закрыли форму
+                        //form was closed
                     }
-                    await Task.Delay(1); //минипауза, чтоб не блокировало интерфейс
+                    await Task.Delay(1); //do not block user interface
                 }
             }
         }
 
-        //рисуем функцию во всю доступную область
+        //draw an arbitrary function
         private void DrawF(Graphics g, Func<double, double> f, double XSCALE, double YSCALE, double xShift)
         {
-            //масштабирование (сколько пикселей в единичке)
+            //how many pixels in interval [0, 1]
             //double XSCALE = 100;
             //double YSCALE = Math.PI * XSCALE;
 
-            //положение центра
+            //position of (0,0) coordinate
             double XCENTER = this.Width / 2;
             double YCENTER = this.Height / 2;
 
-            //размер "бумаги"
+            //drawing area
             double XSIZE = this.Width;
             double YSIZE = this.Height;
 
             Pen pen = new Pen(Brushes.Black, 1);
 
-            //рисуем оси
+            //show axes
             g.DrawLine(pen, 0, (int)YCENTER, (int)XSIZE, (int)YCENTER);
             g.DrawLine(pen, (int)XCENTER, 0, (int)XCENTER, (int)YSIZE);
             DrawString(g, "0", (int)XCENTER, (int)YCENTER);
 
-            //насечки на осях
-            double DASH = 10; //размер насечки
+            //axis marks
+            double DASH = 10; //mark size
             for (int i = 1; XCENTER + i * XSCALE < XSIZE; i++)
             {
                 g.DrawLine(pen, (int)(XCENTER + i * XSCALE), (int)(-DASH / 2 + YCENTER), (int)(XCENTER + i * XSCALE), (int)(DASH / 2 + YCENTER));
@@ -93,15 +93,15 @@ namespace WinFormsApp1
                 DrawString(g, (-i).ToString(), (int)XCENTER, (int)(YCENTER + i * YSCALE));
             }
 
-            //рисуем функцию линиями: f(x) -> f(x + XSTEP)
+            //draw funtion with lines: f(x) -> f(x + XSTEP)
             double xStart = -XCENTER / XSCALE - xShift;
             double xEnd = (XSIZE - XCENTER) / XSCALE;
-            double XSTEP = .05; //шаг по x
+            double XSTEP = .05; //x step
             bool isFirst = true;
-            double x1 = 0, y1 = 0; //предыдущая точка
+            double x1 = 0, y1 = 0; //previous point
             for (double x = xStart; x < xEnd + XSTEP; x += XSTEP)
             {
-                double y = -f(x); //y нужно зеркалить (на бумаге y растет вверх, а на экране вниз)
+                double y = -f(x); //mirror y coordinate
                 if (double.IsNaN(y))
                     continue;
                 if (!isFirst)
@@ -112,7 +112,7 @@ namespace WinFormsApp1
             }
         }
 
-        //написать текст в определенном месте
+        //show a text at specific coordinate
         private void DrawString(Graphics g, string str, int x, int y)
         {
             SizeF a = TextRenderer.MeasureText(str, this.Font);
